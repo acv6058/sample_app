6e0553912a80c7242fd2058e828353f01ff2e0f4
acv6058 <alvinvargas002@gmail.com>
2014-02-24 23:29:19 +0800

    added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def logo
+  	image_tag("logo.png", :alt => "Samp App", :class => "round")
+  end
 end
